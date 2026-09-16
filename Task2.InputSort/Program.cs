b6f59f8e7d655feb750b0eb4dbd8112e2de4836d
zayarniy <zaazaa@yandex.ru>
2026-09-16 13:48:45 +0300

    Update Program.cs

--- Task2.InputSort/Program.cs
+++ Task2.InputSort/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+namespace Task2
+{
+   class Program
+   {
+     static void Main()
+     {
+       Console.WriteLine("Hello, World!");
+     }
+   }
+}
